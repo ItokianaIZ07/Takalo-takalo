2c2da5d120b41cff02401306c0fe668736603d3a
brandon0638 <brandonandria06@gmail.com>
2026-02-12 12:35:21 +0300

    creation de la table objet et insertion de donnee Categorie & Objet

---
+++ database/20260212-01-objet.sql
@@ -0,0 +1,28 @@
+create table Objet(
+    idObjet int primary key auto_increment,
+    idCategorie int not null,
+    idUser int not null, 
+    nomObjet varchar(255) not null, 
+    description text, 
+    prix_estimatif int not null,
+    img varchar(700)
+);
+
+INSERT INTO Category(name) VALUES 
+("Ordinateur"),
+("Vélo"),
+("Téléphone");
+
+
+INSERT INTO Objet (idCategorie, idUser, nomObjet, description, prix_estimatif, img) VALUES
+(1, 1, 'Ordinateur portable HP', 'Ordinateur en bon état, 8Go RAM, SSD 256Go', 1200000, 'images/hp_laptop.jpg'),
+
+(1, 2, 'MacBook Pro 2019', 'Très bon état, batterie neuve', 2500000, 'images/macbook.jpg'),
+
+(2, 1, 'Vélo tout terrain', 'VTT presque neuf, utilisé 3 mois', 600000, 'images/vtt.jpg'),
+
+(3, 3, 'Téléphone Samsung Galaxy S21', 'Écran intact, fonctionne parfaitement', 900000, 'images/s21.jpg'),
+
+(3, 2, 'iPhone 12', '64Go, état correct', 1100000, 'images/iphone12.jpg');
+
+
